cdccf58c70e5a1c10a936cf12b7ddd014246a9e9
Fernando <fjmarson@gmail.com>
2024-11-15 08:40:35 -0300

    first commit

---
+++ src/main/java/com/marson/GratitudeMessage/GratitudeMessageApplication.java
@@ -0,0 +1,13 @@
+package com.marson.GratitudeMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marson/GratitudeMessage/GratitudeMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marson.GratitudeMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratitudeMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
